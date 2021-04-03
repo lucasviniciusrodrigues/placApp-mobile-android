e14da29f644b9b54d9c6cad672683b792e3b6fad
lucasviniciusrodrigues <lucas.vinicius.rodrigues@hotmail.com>
2021-04-03 19:27:07 -0300

    Adding game-set and winner message

--- app/src/main/java/com/ghostapps/placapp/GameScoreActivity.kt
+++ app/src/main/java/com/ghostapps/placapp/GameScoreActivity.kt
@@ -1,6 +1,7 @@
 package com.ghostapps.placapp
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_score_game.*
 
@@ -26,8 +27,10 @@ class GameScoreActivity: AppCompatActivity() {
 
         gameScoreHomeIncrease.setOnClickListener { 
             homeTeamScore++
-            if(checksetChange(homeTeamScore, awayTeamScore))
+            if(checksetChange(homeTeamScore, awayTeamScore)){
                 homeTeamSetScore++
+                checkHomeWin()
+            }
             updateScore()
         }
         
@@ -38,8 +41,9 @@ class GameScoreActivity: AppCompatActivity() {
         
         gameScoreAwayIncrease.setOnClickListener {  
             awayTeamScore++
-            if(checksetChange(awayTeamScore, homeTeamScore))
+            if(checksetChange(awayTeamScore, homeTeamScore)){
                 awayTeamSetScore++
+            }
             updateScore()
         }
         
@@ -53,7 +57,36 @@ class GameScoreActivity: AppCompatActivity() {
         }
     }
 
+    private fun checkHomeWin() {
+        if(checkWinner(homeTeamSetScore, awayTeamSetScore)){
+            Toast.makeText(this, "O time " + intent.getStringExtra(TEAM_HOME_NAME) + " venceu", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun checkAwayWin() {
+        if(checkWinner(awayTeamSetScore, homeTeamSetScore)){
+            Toast.makeText(this, "O time " + intent.getStringExtra(TEAM_AWAY_NAME) + " venceu", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun checkWinner(winnerTeamSetScore: Int, anotherTeamSetScore: Int): Boolean{
+        if(winnerTeamSetScore - anotherTeamSetScore > 2){
+            homeTeamSetScore = 0
+            awayTeamSetScore = 0
+            return true;
+        }
+        return false;
+    }
+
     private fun checksetChange(newScore: Int, oldScore: Int): Boolean {
+
+        if(homeTeamSetScore == 2 && awayTeamSetScore == 2
+            && newScore >= 15 && newScore - oldScore > 1){
+            homeTeamScore = 0;
+            awayTeamScore = 0;
+            return true;
+        }
+
         if(newScore >= 25 && newScore - oldScore > 1){
             homeTeamScore = 0;
             awayTeamScore = 0;
